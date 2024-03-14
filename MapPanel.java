d1e7fbdbbc1930bb76f962109db446cdbc893ce7
Ahmet Taramis (k22038914) <ahmet.taramis@kcl.ac.uk>
2024-03-14 07:04:41 +0000

    Add files via upload

--- MapPanel.java
+++ MapPanel.java
@@ -1,4 +1,4 @@
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.control.Label;
 
 /**
@@ -8,7 +8,7 @@ public class MapPanel extends Panel {
     
     public MapPanel() {
         super();
-        this.panel = new BorderPane(); // Change the layout style for the panel
+        this.panel = new Pane(); // Change the layout style for the panel here
         setupPanel();
     }
     
--- WelcomePanel.java
+++ WelcomePanel.java
@@ -1,4 +1,4 @@
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.control.Label;
 
 /**
@@ -8,13 +8,13 @@ public class WelcomePanel extends Panel {
     
     public WelcomePanel() {
         super();
-        this.panel = new BorderPane(); // Change the layout style for the panel
+        this.panel = new Pane(); // Change the layout style for the panel here
         setupPanel();
     }
     
     @Override
     protected void setupPanel() {
         panel.getChildren().add(new Label("Welcome Panel")); // Test statement to show panel works
-        // Implement the setup for the Welcome panel here
+        // Implement the setup for the Map panel here
     }
 }
