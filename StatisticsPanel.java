fffbfd2a7a8d9b4675c26273c7216f1d9ab7d2d6
Khadija Hashim (k23104223) <khadija.hashim@kcl.ac.uk>
2024-03-23 01:37:25 +0000

    Update StatisticsPanel.java

--- StatisticsPanel.java
+++ StatisticsPanel.java
@@ -5,7 +5,6 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import java.time.*;
 import java.time.LocalDate;
-
 import java.util.ArrayList;
 import javafx.application.Platform;
 import javafx.geometry.Pos;
@@ -16,12 +15,16 @@ import java.util.function.Function;
  * The Statistics Panel designed to show various COVID-19 statistics.
  */
 public class StatisticsPanel extends Panel {
+    // containers for buttons and statistics view
     private VBox statistic1Container;
     private VBox statistic2Container;
     private VBox statistic3Container;
+    // buttons 
     private Button btnNextStat;
     private Button btnPrevStat;
+    // counter to keep track of different stats
     private int currentStatIndex = 0;
+    // covid data 
     private ArrayList<CovidData> covidDataList;
     
     public StatisticsPanel() {
@@ -32,43 +35,45 @@ public class StatisticsPanel extends Panel {
     
     @Override
     protected void setupPanel() {
+        //making containers
         BorderPane root = new BorderPane();
-        
         statistic1Container = new VBox();
         statistic2Container = new VBox();
         statistic2Container.setStyle("-fx-background-color: white;");
         statistic3Container = new VBox();
         
         setupNavigationButtons();
+        
+        //adding buttons to containers
         statistic1Container.getChildren().add(btnPrevStat);
         statistic3Container.getChildren().add(btnNextStat);
         
-        root.setAlignment(statistic2Container, Pos.CENTER);
-        root.setMargin(statistic2Container, new Insets(10));
-        
-        statistic1Container.setPadding(new Insets(10));
-        statistic2Container.setPadding(new Insets(70)); // Adjust the inset value as needed
-        
+        //setting the components in the right place
         statistic1Container.setAlignment(Pos.CENTER);
+        statistic2Container.setAlignment(Pos.CENTER);
         statistic3Container.setAlignment(Pos.CENTER);
-
-
-        // Add constraints to ensure statistic2Container fills the center
-        root.setAlignment(statistic3Container, Pos.CENTER_RIGHT);
-    
-        // Add label for Statistics Panel at the top
+        
+        //add label for Statistics Panel at the top
         Label titleLabel = new Label("STATISTICS");
-        titleLabel.setStyle("-fx-font-size: 50px;-fx-alignment : center;-fx-font-family: 'WingDings MS'"); // Set font size
+        titleLabel.setStyle("-fx-font-size: 30px;-fx-alignment : center;-fx-font-family: 'WingDings MS'"); // Set font size
+        root.setAlignment(titleLabel, Pos.CENTER);
         root.setTop(titleLabel);
         
+        //setting the different containers in different parts of the border pane
         root.setLeft(statistic1Container);
         root.setCenter(statistic2Container);
         root.setRight(statistic3Container);
         
+        //setting it to fit the window
+        root.setPrefSize(800,600);
+        
         setupStatistics(currentStatIndex);
-        panel.getChildren().addAll(root); // Test statement to show panel works
+        
+        // add the borderpane to the panel
+        panel.getChildren().addAll(root);
     }
     
+    // STATISTICS FUNCTIONS USED
     private double calculateAverageTransitGMR() {
         double total = 0;
         for(CovidData data:filteredRecords){
@@ -100,6 +105,7 @@ public class StatisticsPanel extends Panel {
         }
         return total/33; //Divided by 33 as that is the amount of boroughs, therefore giving average cases per borough
     }
+    
     // adds functionality to navigation buttons
     private void setupStatistics(int currentStatIndex) {
         Platform.runLater(() -> {
@@ -108,55 +114,57 @@ public class StatisticsPanel extends Panel {
     
             // Create a label for the statistic
             Label statisticLabel = new Label();
-            statisticLabel.setStyle("-fx-font-size: 20px;"); // Set font size
+
             statisticLabel.setAlignment(Pos.CENTER); // Align the label to the center
             
             // Update statistics labels
             switch (currentStatIndex) {
                 case 0:
                     // Statistic 1: Average transit GMR
-                    statisticLabel.setText("Average Transit Google Mobility: \n" + calculateAverageTransitGMR());
+                    statisticLabel.setText("Average Transit Google Mobility:\n" + calculateAverageTransitGMR());
                     break;
                 case 1:
                     // Statistic 2: Average grocery/pharmacy GMR
-                    statisticLabel.setText("Average Grocery/Pharmacy Google Mobility: \n" + calculateAverageGroceryPharmacyGMR());
+                    statisticLabel.setText("Average Grocery Pharmacy \nGoogle Mobility:\n" + calculateAverageGroceryPharmacyGMR());
                     break;
                 case 2:
                     // Statistic 3: Total deaths
-                    statisticLabel.setText("Total Deaths: \n" + calculateTotalDeaths());
+                    statisticLabel.setText("Total Deaths:\n" + calculateTotalDeaths());
                     break;
                 case 3:
                     // Statistic 4: 
-                    statisticLabel.setText("Average Cases Per Borough: \n" + calculateAverageTotalCases());
+                    statisticLabel.setText("Average Cases Per Borough:\n" + calculateAverageTotalCases());
                     break;
                 default:
                     break;
             }
             
-            // Add the statistic label to container 2
-            statisticLabel.setStyle("-fx-font-size: 60px; -fx-alignment : center;-fx-font-family: 'WingDings MS'; "); 
-            statisticLabel.setPrefWidth(685);
-            statisticLabel.setPrefHeight(700);
+            // Add the statistic label to container 2 with specifications
+            statisticLabel.setStyle("-fx-font-size: 30px; -fx-alignment : center;-fx-font-family: 'WingDings MS'; "); 
+            statisticLabel.setPrefWidth(700);
+            statisticLabel.setPrefHeight(300);
             statistic2Container.getChildren().add(statisticLabel);
         });
     }
     
+    // setting up the next and previous buttons
     private void setupNavigationButtons(){
         btnNextStat = new Button("->");
         btnPrevStat = new Button("<-");
         
-        btnNextStat.setPrefWidth(400); 
-        btnNextStat.setPrefHeight(900); 
-        btnPrevStat.setPrefWidth(400); 
-        btnPrevStat.setPrefHeight(900); 
+        btnNextStat.setPrefWidth(150); 
+        btnNextStat.setPrefHeight(580); 
+        btnPrevStat.setPrefWidth(150); 
+        btnPrevStat.setPrefHeight(580); 
         
-        btnNextStat.setStyle("-fx-font-size: 140px;"); 
-        btnPrevStat.setStyle("-fx-font-size: 140px;"); 
+        btnNextStat.setStyle("-fx-font-size: 30px;"); 
+        btnPrevStat.setStyle("-fx-font-size: 30px;"); 
     
         btnNextStat.setOnAction(e-> navigateToNextStatistic());
         btnPrevStat.setOnAction(e-> navigateToPreviousStatistic());
     }
     
+    //functions used for the setupNavigationButtons() function
     private void navigateToNextStatistic() {
         currentStatIndex = (currentStatIndex + 1) % 4;
         setupStatistics(currentStatIndex);
@@ -172,6 +180,5 @@ public class StatisticsPanel extends Panel {
     protected void setFilteredRecords(List<CovidData> filteredRecords){
         super.setFilteredRecords(filteredRecords);
         setupStatistics(currentStatIndex);
-    }
-    
+    }    
 }
